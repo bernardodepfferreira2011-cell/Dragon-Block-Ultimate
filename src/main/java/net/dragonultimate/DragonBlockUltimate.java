6ae378c2f89f15fc049ccbc74af032e2e59d9bea
bernardodepfferreira2011-cell <bernardodepfferreira2011@gmail.com>
2026-06-12 00:00:02 +0000

    setup 1

---
+++ src/main/java/net/dragonultimate/DragonBlockUltimate.java
@@ -0,0 +1,51 @@
+package net.dragonultimate;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(DragonBlockUltimate.MOD_ID)
+public class DragonBlockUltimate {
+    public static final String MOD_ID = "dragonblockultimate";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public DragonBlockUltimate(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+        public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+
+}
